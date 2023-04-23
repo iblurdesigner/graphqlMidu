6e236b9aecf096259201509a6e2383f9621785ab
iblurdesigner <nevomen@gmail.com>
2023-04-22 21:19:39 -0500

    Queries compuestas

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 import { ApolloServer } from '@apollo/server';
 import { GraphQLError } from 'graphql';
 import { startStandaloneServer } from '@apollo/server/standalone';
+import axios from 'axios'
 
 import {v1 as uuid} from 'uuid'
 
@@ -68,13 +69,15 @@ const typeDefs = `#graphql
 const resolvers = {
     Query: {
         personCount: () => persons.length,
-        allPersons: (root, args) => {
-          if(!args.phone) return persons
+        allPersons: async(root, args) => {
+          const {data: personsFromRestApi} = await axios.get('http://localhost:3000/persons')
+
+          if(!args.phone) return personsFromRestApi
 
           const byPhone = person => 
             args.phone === "YES" ? person.phone : !person.phone
           
-          return persons.filter(byPhone)
+          return personsFromRestApi.filter(byPhone)
           },
         findPerson: (root, args) => {
             const {name} = args
